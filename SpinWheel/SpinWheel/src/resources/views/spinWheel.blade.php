55900a903f8ae4225666f768ccf1317d65535a72
hagarwal-velsof <58728963+hagarwal-velsof@users.noreply.github.com>
2022-10-20 17:57:52 +0530

    Delete SpinWheel/SpinWheel directory

--- SpinWheel/SpinWheel/src/resources/views/spinWheel.blade.php
+++
@@ -1,1 +0,0 @@
-This is a spin wheel view.
--- SpinWheel/SpinWheel/src/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-   
-use SpinWheel\SpinWheel\Http\Controllers\SpinWheelMainController;
-use Illuminate\Support\Facades\Route;
-   
-Route::get('testspin', [SpinWheelMainController::class, 'SpinTheWheel'])->name('testspin');
-?>
